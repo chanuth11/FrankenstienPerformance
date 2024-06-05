57f8f4e606bc42ae35387eebcac4c052ad3ff466
Chanuth <chanuthw55@gmail.com>
2024-06-05 12:32:42 -0400

    CV python script
    Added CV script for the listener side

---
+++ CV Note Reader/main.py
@@ -0,0 +1,18 @@
+'''
+
+    By: Chanuth Weeraratna
+
+    Latest Revision: 6/5/24 
+
+    Code referenced from https://www.youtube.com/watch?v=O3b8lVF93jU
+
+    This program takes any Rousseau video or any video siilar to his format, and matches his fingers
+    to the piano note being played to send a signal to the ESP32 to drive the motor corresponding to   
+    the finger playing the note
+
+    Features to add: 
+    - Determining the sound level of each note being played to determine the "finger pressure" or
+      motor speed 
+
+'''
+
